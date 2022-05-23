e66d499c6218af0dfc328e4a4bed5cf75079ed48
aberraxao <diana_milu@live.com.pt>
2022-05-23 23:05:36 +0100

    Template files added

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Test {
+   public static void main(String[] args) {
+
+      // read in the terms from a file
+      String filename = args[0];
+      In in = new In(filename);
+      int n = in.readInt();
+      Term[] terms = new Term[n];
+      for (int i = 0; i < n; i++) {
+         long weight = in.readLong();           // read the next weight
+         in.readChar();                         // scan past the tab
+         String query = in.readLine();          // read the next query
+         terms[i] = new Term(query, weight);    // construct the term
+      }
+
+      // read in queries from standard input and print the top k matching terms
+      int k = Integer.parseInt(args[1]);
+      Autocomplete autocomplete = new Autocomplete(terms);
+      while (StdIn.hasNextLine()) {
+         String prefix = StdIn.readLine();
+         Term[] results = autocomplete.allMatches(prefix);
+         StdOut.printf("%d matches\n", autocomplete.numberOfMatches(prefix));
+         for (int i = 0; i < Math.min(k, results.length); i++)
+            StdOut.println(results[i]);
+      }
+   }
+
+}
