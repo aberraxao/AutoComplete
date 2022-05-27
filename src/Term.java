2fb365d26259226af965ed2f0ff3b73b1f2bd64d
aberraxao <diana_milu@live.com.pt>
2022-05-27 04:40:32 +0100

    Tradução das docstrings, utilização da função 'stdout' de algs4 e 'try... catch'

--- src/Term.java
+++ src/Term.java
@@ -1,148 +1,151 @@
 package src;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * Implements an Autocomplete Term: a string query and its respective weight.
- * 
+ * Implementa a classe Term com a String query (consulta) e o respetivo long weight (peso).
+ *
  * @author Diana Amaro
  * @author Diogo Ribeiro
- *
  */
 public class Term implements Comparable<Term> {
 
-	private String query;
-	private long weight;
+   private String query;
+   private long weight;
 
-	/**
-	 * Initializes a term with the given query string and weight.
-	 * 
-	 * @param query		Term's query
-	 * @param weight	Term's weight
-	 */
-	public Term(String query, long weight)
-	{
-		if(query == null)
-			throw new IllegalArgumentException("Query cannot be null");
-		if(weight < 0)
-			throw new IllegalArgumentException("Weight cannot be negative");
+   /**
+    * Inicializa o Term com a respetiva query e weight.
+    *
+    * @param query  consulta do termo
+    * @param weight peso do termo
+    */
+   public Term(String query, long weight) {
+      if (query == null) throw new IllegalArgumentException("A consulta não pode ser nula");
+      if (weight < 0) throw new IllegalArgumentException("O peso não pode ser negativo");
 
-		this.query = query;
-		this.weight = weight;
-	}
+      this.query = query;
+      this.weight = weight;
+   }
 
-	/**
-	 * Compares the two terms in descending order by weight.
-	 * 
-	 * @return	Comparator by weight descending order
-	 */
-	public static Comparator<Term> byReverseWeightOrder()
-	{
-		return (thisTerm, thatTerm) -> Long.compare(thatTerm.weight, thisTerm.weight);
-	}
+   /**
+    * Compara dois termos por ordem descendente de peso
+    *
+    * @return Comparator ordenado por ordem de peso descendente
+    */
+   public static Comparator<Term> byReverseWeightOrder() {
+      return (thisTerm, thatTerm) -> Long.compare(thatTerm.weight, thisTerm.weight);
+   }
 
-	/**
-	 * Compares the two terms in lexicographic order,
-	 * but using only the first r characters of each query.
-	 * 
-	 * @param r number of characters to compare
-	 * @return	Comparator by prefix order
-	 */
-	public static Comparator<Term> byPrefixOrder(int r)
-	{
-		if(r < 0)
-			throw new IllegalArgumentException("r cannot be negative");
+   /**
+    * Compara dois termos por ordem lexicográfica por string de consulta (ordem natural), comparando apenas os seus
+    * primeiros 'r' caracteres.
+    *
+    * @param r número de caracteres a comparar
+    * @return Comparator ordenado por ordem lexicográfica
+    */
+   public static Comparator<Term> byPrefixOrder(int r) {
 
-        return (thisTerm, thatTerm) ->
-        {
-        	//check if each query is longer than the specified prefix length (r) and trim it down
-            String thisQuery = thisTerm.query.length() < r ? thisTerm.query : thisTerm.query.substring(0, r);
-            String thatQuery = thatTerm.query.length() < r ? thatTerm.query : thatTerm.query.substring(0, r);
-            
-            return thisQuery.compareTo(thatQuery);
-        };
-	}
-	
-	/**
-	 * Compares the two terms in lexicographic order by query.
-	 * 
-	 * @return	this Term to that Term comparison as a number: negative if less, zero if equal, positive if greater
-	 */
-	public int compareTo(Term that)
-	{
-		return this.query.compareTo(that.query);
-	}
+      if (r < 0) throw new IllegalArgumentException("O número de caracteres não pode ser negativo");
 
-	/**
-	 * Returns a string representation of this term in the following format:
-	 * the weight, followed by a tab, followed by the query.
-	 * 
-	 * @return	Term formatted as a string
-	 */
-	public String toString()
-	{
-		return this.weight + "\t" + this.query;
-	}
+      return (thisTerm, thatTerm) -> {
 
-	/**
-	 * unit testing (required)
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		// Test Constructor Exceptions (uncomment for testing)
-		System.out.println("Constructor Exception - START");
-//		Term eTerm1 = new Term(null, 15);
-//		Term eTerm2 = new Term("Tokyo, City", -1);
-		System.out.println("Constructor Exception - END");
-		System.out.println();
-	
-		// Initialize several test Terms
-		System.out.println("Initialize Terms - START");
-		Term[] terms = new Term[4];
-		terms[0] = new Term("Seoul, South Korea", 10349312);
-		terms[1] = new Term("Buenos Aires, Argentina", 13076300);
-		terms[2] = new Term("Mumbai, India", 12691836);
-		terms[3] = new Term("Jakarta, Indonesia", 8540121);
-		System.out.println("Initialize Terms - END");
-		System.out.println();
-		
-		// Test toString
-		System.out.println("toString - START");
-		System.out.println("Term: "+ terms[0].toString());
-		System.out.println("toString - END");
-		System.out.println();
-		
-		// Test compareTo
-		System.out.println("compareTo - START");
-		System.out.println("compare " + terms[0].query + " to " + terms[1].query + " = " + terms[0].query.compareTo(terms[1].query));
-		System.out.println("compare " + terms[0].query + " to " + terms[0].query + " = " + terms[0].query.compareTo(terms[0].query));
-		System.out.println("compare " + terms[1].query + " to " + terms[0].query + " = " + terms[1].query.compareTo(terms[0].query));
-		System.out.println("compareTo - END");
-		System.out.println();
-		
-		// byPrefixOrder Tester
-		System.out.println("byPrefixOrder - START");
-		Arrays.sort(terms, byPrefixOrder(2));
-		for(int i = 0; i < terms.length; i++)
-		{
-			System.out.println(terms[i].toString());
-		}
-		System.out.println("byPrefixOrder - END");
-		
-		System.out.println();
-		
-		// Test	byReverseWeightOrder
-		System.out.println("byReverseWeightOrder - START");
-		Arrays.sort(terms, byReverseWeightOrder());
-		for(int i = 0; i < terms.length; i++)
-		{
-			System.out.println(terms[i].toString());
-		}
-		System.out.println("byReverseWeightOrder - END");
-		System.out.println();
-		
-	}
+         // Se o comprimento da consulta for superior ao número de caracteres, considera apenas os primeiros r
+         String thisQuery = thisTerm.query.length() < r ? thisTerm.query : thisTerm.query.substring(0, r);
+         String thatQuery = thatTerm.query.length() < r ? thatTerm.query : thatTerm.query.substring(0, r);
+
+         return thisQuery.compareTo(thatQuery);
+      };
+   }
+
+   /**
+    * Compara dois termos por ordem lexicográfica por string de consulta (ordem natural)
+    *
+    * @return o valor do termo this em comparação com o termo that: negativo se this menor, zero se iguais,
+    * positivo se this maior
+    */
+   public int compareTo(Term that) {
+      return this.query.compareTo(that.query);
+   }
+
+   /**
+    * Devolve uma respresentação do termo no formato:
+    * peso, seguido da tabulação, seguido da consulta
+    *
+    * @return Term com formato string
+    */
+   public String toString() {
+      return String.format("%d\t%s", this.weight, this.query);
+   }
+
+   /**
+    * Testes unitários
+    *
+    * @param args Nome do ficheiro de entrada
+    */
+   public static void main(String[] args) {
+      StdOut.println("Inicia um conjunto de testes do módulo Term...\n");
+
+      StdOut.println("INÍCIO: Testes das exceções do construtor");
+      try {
+         Term terms = new Term(null, 15);
+      } catch (IllegalArgumentException e) {
+         StdOut.println("Exceção da consulta testada");
+      }
+      try {
+         Term terms = new Term("Tokyo, City", -1);
+      } catch (IllegalArgumentException e) {
+         StdOut.println("Exceção do peso testada");
+      }
+      StdOut.println("FIM: Testes das exceções do construtor\n");
+
+      // Lê os termos do ficheiro recebido em args[0] para prosseguir com os testes
+      String filename = args[0];
+      In in = new In(filename);
+      int n = in.readInt();
+      Term[] terms = new Term[n];
+      for (int i = 0; i < n; i++) {
+         long weight = in.readLong();           // lê o próximo peso
+         in.readChar();                         // lê a tab
+         String query = in.readLine();          // lê a próxima consulta
+         terms[i] = new Term(query, weight);    // contrói o termo
+      }
+
+      StdOut.println("INÍCIO: Testa toString");
+      StdOut.printf("Term: %s\n", terms[0].toString());
+      StdOut.println("FIM: Testa toString\n");
+
+      StdOut.println("INÍCIO: Testa compareTo");
+      StdOut.printf("Comparação '%s' vs '%s' = %d\n",
+          terms[0].query, terms[1].query, terms[0].query.compareTo(terms[1].query));
+      StdOut.printf("Comparação '%s' vs '%s' = %d\n",
+          terms[0].query, terms[0].query, terms[0].query.compareTo(terms[0].query));
+      StdOut.printf("Comparação '%s' vs '%s' = %d\n",
+          terms[1].query, terms[0].query, terms[1].query.compareTo(terms[0].query));
+      StdOut.println("FIM: Testa compareTo\n");
+
+      StdOut.println("INÍCIO: Testa byPrefixOrder");
+      try {
+         byPrefixOrder(-5);
+      } catch (IllegalArgumentException e) {
+         StdOut.println("Exceção do comprimento testada");
+      }
+      Arrays.sort(terms, byPrefixOrder(20));
+      StdOut.println("Se existirem, imprime os primeiros 10 termos ordenados.");
+      for (int i = 0; (i < terms.length && i < 10); i++)
+         StdOut.println(terms[i].toString());
+      StdOut.println("FIM: Testa byPrefixOrder\n");
+
+      StdOut.println("INÍCIO: Testa byReverseWeightOrder");
+      Arrays.sort(terms, byReverseWeightOrder());
+      StdOut.println("Se existirem, imprime os primeiros 10 termos ordenados.");
+      for (int i = 0; (i < terms.length && i < 10); i++)
+         StdOut.println(terms[i].toString());
+      StdOut.println("FIM: Testa byReverseWeightOrder\n");
+
+      StdOut.println("Termina o conjunto de testes do módulo Term");
+   }
 }
